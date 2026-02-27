197bda758b1b0c245671f866222fd3d05edb8855
Rohit Shivsharan Subhedar <Rohitsubhe01@gmail.com>
2026-02-27 16:06:06 +0530

    commit

---
+++ SpringBootMVCExp3/src/main/java/com/test/SpringBootMvcExp3Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcExp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcExp3Application.class, args);
+	}
+
+}
