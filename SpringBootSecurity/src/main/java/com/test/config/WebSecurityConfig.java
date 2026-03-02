2a9808b8ce47580936a4983908bd9aeee1f110b3
Rohit Shivsharan Subhedar <Rohitsubhe01@gmail.com>
2026-03-02 12:07:39 +0530

    commit

---
+++ SpringBootSecurity/src/main/java/com/test/config/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity 
+public class WebSecurityConfig {
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.builder().username("USER")
+										.password(passwordEncoder().encode("abc123"))
+										.roles("USER")
+										.build();
+		UserDetails admin = User.builder().username("ADMIN")
+										.password(passwordEncoder().encode("java"))
+										.roles("ADMIN","USER")
+										.build();
+		UserDetails hr = User.builder().username("HR")
+				.password(passwordEncoder().encode("hr"))
+				.roles("ADMIN","USER")
+				.build();
+		UserDetails emp = User.builder().username("Employee")
+				.password(passwordEncoder().encode("emp"))
+				.roles("ADMIN","USER")
+				.build();
+		return new InMemoryUserDetailsManager(user, admin, hr, emp);
+	}
+	@Bean
+	protected PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	// csrf ?? => a policy that aims to prevent cross site request forger protection web applications.
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) {
+		return http.csrf(AbstractHttpConfigurer :: disable)
+				.authorizeHttpRequests(request -> request.requestMatchers("/login")
+											.permitAll()
+											.requestMatchers("/**")
+											.authenticated())
+				.formLogin(form -> form.loginPage("/login")
+						.defaultSuccessUrl("/")
+						.failureUrl("/login?error=true")
+						.permitAll()
+						)
+				.logout(config -> 
+						config.logoutUrl("/logout")
+						.logoutSuccessUrl("/login"))
+				.build();
+	} 
+}
